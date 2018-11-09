f24a64aa96c2fd2d9e1f55006f5f5f0daad5f176
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-09 18:48:10 -0400

    quetao 6

---
+++ prova06_sistemacliente/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace prova06_sistemacliente
+{
+    public class Cliente
+    {
+        string cidade;
+
+        public Cliente(string cidade)
+        {
+            this.cidade = cidade;
+        }
+
+            public string nome { get; set; }
+        public string Cidadeinicializada(){
+            return cidade;
+        }
+    }
+}
+
---
+++ prova06_sistemacliente/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prova06_sistemacliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente("Manaus");
+
+            Console.Write("Informe seu Nome: ");
+            cliente.nome = Console.ReadLine();
+
+            Console.WriteLine("{0} eh de {1}", cliente.nome, cliente.Cidadeinicializada());
+        }
+    }
+}
